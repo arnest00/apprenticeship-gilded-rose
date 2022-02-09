e152de15956c971bc8d60029e1fbc606c39a4d0d
Osmond <osmond@heysparkbox.com>
2022-02-09 13:29:08 -0700

    test: Add test support for conjured items

--- src/inventory/gilded_rose.spec.js
+++ src/inventory/gilded_rose.spec.js
@@ -19,51 +19,63 @@ describe('`updateQuality`', () => {
     expect(standardItem.quality).toBe(0);
   });
 
-  it('increments quality by 1 for Aged Brie when sell_in is above 0', () => {
+  it('increments quality by 1 for Aged Brie with sell_in > 0', () => {
     const agedBrie = new Item('Aged Brie', 2, 0);
     updateQuality([agedBrie]);
     expect(agedBrie.quality).toBe(1);
   });
 
-  it('quality of Aged Brie is never more than 50', () => {
+  it('quality of Aged Brie is never > 50', () => {
     const agedBrie = new Item('Aged Brie', 0, 50);
     updateQuality([agedBrie]);
     expect(agedBrie.quality).toBe(50);
   });
 
-  it('quality of Sulfuras is always 80', () => {
+  it('quality of Sulfuras is always = 80', () => {
     const sulfuras = new Item('Sulfuras, Hand of Ragnaros', 0, 80);
     updateQuality([sulfuras]);
     expect(sulfuras.quality).toBe(80);
   });
 
-  it('increments quality by 1 for TAFKAL80ETC passes when sell_in is more than 10', () => {
+  it('increments quality by 1 for TAFKAL80ETC passes with sell_in > 10', () => {
     const passes = new Item('Backstage passes to a TAFKAL80ETC concert', 11, 20);
     updateQuality([passes]);
     expect(passes.quality).toBe(21);
   });
 
-  it('increments quality by 2 for TAFKAL80ETC passes when sell_in is less than 10', () => {
+  it('increments quality by 2 for TAFKAL80ETC passes with sell_in < 10', () => {
     const passes = new Item('Backstage passes to a TAFKAL80ETC concert', 9, 20);
     updateQuality([passes]);
     expect(passes.quality).toBe(22);
   });
 
-  it('increments quality by 3 for TAFKAL80ETC passes when sell_in is less than 5', () => {
+  it('increments quality by 3 for TAFKAL80ETC passes with sell_in < 5', () => {
     const passes = new Item('Backstage passes to a TAFKAL80ETC concert', 4, 20);
     updateQuality([passes]);
     expect(passes.quality).toBe(23);
   });
 
-  it('sets quality to 0 for TAFKAL80ETC passes when sell_in is less than 0', () => {
+  it('sets quality to 0 for TAFKAL80ETC passes with sell_in < 0', () => {
     const passes = new Item('Backstage passes to a TAFKAL80ETC concert', 0, 20);
     updateQuality([passes]);
     expect(passes.quality).toBe(0);
   });
 
-  it('incremenets quality by 2 for Aged Brie with sell_in less than 0 and quality less than 50', () => {
+  it('incremenets quality by 2 for Aged Brie with sell_in < 0 and quality < 50', () => {
     const standardItem = new Item('Aged Brie', -5, 45);
     updateQuality([standardItem]);
     expect(standardItem.quality).toBe(47);
+  });
+
+  it('decrements quality by 2 for a conjured item with sell_in > 0', () => {
+    const conjuredItem = new Item('Conjured Dagger', 10, 24);
+    updateQuality([conjuredItem]);
+    expect(conjuredItem.quality).toBe(22);
+  });
+
+  it('decrements quality by 4 for a conjured item with negative sell_in', () => {
+    const conjuredItem = new Item('Conjured Dagger', -1, 12);
+    updateQuality([conjuredItem]);
+    expect(conjuredItem.quality).toBe(8);
   });
 });
